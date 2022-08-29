f1e6853a4312c656c9b0426cafcccca796429a4a
demidrol <shatilov.demid@gmail.com>
2022-08-29 19:01:30 +0300

    Просто

--- locator_coffeeshop/settings.py
+++ locator_coffeeshop/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-DATA_FOLDER = '../data'
+DATA_FOLDER = '../data/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
